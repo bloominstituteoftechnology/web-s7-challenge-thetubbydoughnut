63451bfec5e00c5809a30fada33f04cba448cf54
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-05-05 16:08:48 -0500

    Got the full name bar working

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -9,6 +9,19 @@ const validationErrors = {
 }
 
 // 👇 Here you will create your schema.
+const pizzaSchema = yup.object().shape({
+  fullName:
+    yup.string().min(3, validationErrors.fullNameTooShort)
+    .max(20, validationErrors.fullNameTooLong)
+    .required(),
+
+  size:
+    yup.string().oneOf(['S','M','L'],
+    validationErrors.sizeIncorrect)
+    .required(),
+})
+
+
 
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
@@ -22,8 +35,52 @@ const toppings = [
 
 
 export default function Form() {
+  const [size, setSize] = useState('')
+  const [fullName, setfullName] = useState('')
+  const [formState, setFormState] = useState({fullName,size});
+  const [errors, setErrors] = useState({});
+  const [disabled, setDisabled] = useState()
+
+  useEffect(() => {
+    pizzaSchema.isValid(formState).then(valid => 
+      setDisabled(!valid)
+    )
+  },[formState])
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    if (name === 'fullName') {
+      setfullName(value)
+    } else if (name === "size") {
+      setSize(value)
+    }
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+
+    yup.reach(pizzaSchema, name)
+    .validate(value)
+    .then(() => setErrors({ ...errors, [name]: ''}))
+    .catch((err) => setErrors({...errors, [name]: err.errors[0]}))
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  
+    pizzaSchema.validate(formState)
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+  }
+  
+  
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Order Your Pizza</h2>
       {true && <div className='success'>Thank you for your order!</div>}
       {true && <div className='failure'>Something went wrong</div>}
@@ -31,22 +88,22 @@ export default function Form() {
       <div className="input-group">
         <div>
           <label htmlFor="fullName">Full Name</label><br />
-          <input placeholder="Type full name" id="fullName" type="text" />
+          <input value={fullName} onChange={handleChange} placeholder="Type full name" id="fullName" name='fullName'type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.fullName && <div className='error'>{errors.fullName}</div>}
       </div>
 
       <div className="input-group">
         <div>
           <label htmlFor="size">Size</label><br />
-          <select id="size">
+          <select id="size" value={size} onChange={(e) => setSize(e.target.value)}>
             <option value="">----Choose Size----</option>
-            <option value="">Small</option>
-            <option value="">Medium</option>
-            <option value="">Large</option>
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {errors.size && <div className='error'>{errors.size}</div>}
       </div>
 
       <div className="input-group">
@@ -61,7 +118,7 @@ export default function Form() {
         ))}
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
-      <input type="submit" disabled='disabled' />
+      <input type="submit" disabled={disabled} />
     </form>
   )
 }
